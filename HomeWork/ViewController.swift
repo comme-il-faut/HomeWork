504fae202808527e9048aea4aef9d40571b3cd86
Rovenko Anton <rovenkoaa@icloud.com>
2023-12-19 20:29:26 +0300

    +

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var plusOneButton: UIButton!
+    @IBOutlet weak var valueCounterLabel: UILabel!
+    
+    var counter = 0 {
+        didSet{
+            valueCounterLabel.text = "Значение счётчика: \(counter)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeValueCounterPlusOne(_ sender: Any) {
+        counter += 1
+    }
+    
 }
 
